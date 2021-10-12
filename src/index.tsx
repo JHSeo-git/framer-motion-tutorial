16585b05093130a7e41e8f0965a76e62b0472cd0
JHSeo <qnfqnfqnf@gmail.com>
2021-10-13 08:55:54 +0900

    Add react boiler plate code with webpack & dev

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>test2</div>
+  </React.StrictMode>,
+  document.getElementById('_root'),
+)
---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+}
