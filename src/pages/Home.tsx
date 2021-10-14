d6204bd586853fee0ea00fcb8a546baba7861f34
JHSeo <qnfqnfqnf@gmail.com>
2021-10-14 21:23:53 +0900

    Add Dummy Card components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { css } from '@linaria/core'
 import AnimatedButton from '../components/AnimatedButton'
+import Card from '../components/Card'
 
 function Home() {
   return (
     <section className={box}>
-      <AnimatedButton />
+      <AnimatedButton name="Button" />
+      {Array.from({ length: 20 }).map((_, idx) => (
+        <Card key={idx}>Card {idx}</Card>
+      ))}
     </section>
   )
 }
