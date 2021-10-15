8069723a2c0041713d52605b4ccaa1828321960e
JHSeo <qnfqnfqnf@gmail.com>
2021-10-15 23:27:08 +0900

    Style Layout background gradient animation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { css } from '@linaria/core'
-import Waves from '../images/bg/waves.svg'
 
 type LayoutProps = {
   children: React.ReactChild
@@ -8,9 +7,7 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <div className={box}>
-      <div className="bg">
-        <Waves height="100%" />
-      </div>
+      <div className="bg"></div>
       <main className={main}>{children}</main>
     </div>
   )
@@ -28,6 +25,28 @@ const box = css`
     bottom: 0;
 
     z-index: -1;
+
+    background: linear-gradient(
+      45deg,
+      var(--pink-color-100),
+      var(--blue-color-100),
+      var(--purple-color-100),
+      var(--cyan-color-100)
+    );
+    background-size: 400% 100%;
+    animation: gradient 10s ease infinite;
+
+    @keyframes gradient {
+      0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+    }
   }
 `
 const main = css``
