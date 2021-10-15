7196ead10cdba5f12c8784ddf13f7c3eeff45f82
JHSeo <qnfqnfqnf@gmail.com>
2021-10-15 21:11:24 +0900

    Add Layout background waves.svg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { css } from '@linaria/core'
+import Waves from '../images/bg/waves.svg'
 
 type LayoutProps = {
   children: React.ReactChild
@@ -7,13 +8,27 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <div className={box}>
+      <div className="bg">
+        <Waves height="100%" />
+      </div>
       <main className={main}>{children}</main>
     </div>
   )
 }
 const box = css`
-  background-color: var(--gray-color-100);
+  /* background-color: var(--gray-color-100); */
   min-height: 100%;
+  position: relative;
+
+  .bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    z-index: -1;
+  }
 `
 const main = css``
 
