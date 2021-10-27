1da7e30d9d22781c929a8de0fb4e748d5f4b9aaa
JHSeo <qnfqnfqnf@gmail.com>
2021-10-27 22:36:03 +0900

    Style fix header scrolled box shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,8 +23,8 @@ function Header() {
   }, [handleScrolling])
 
   return (
-    <nav ref={ref} className={header}>
-      <div className={cx(inner, isTop ? '' : 'outline')}>
+    <nav ref={ref} className={cx(header, isTop ? '' : 'outline')}>
+      <div className={inner}>
         <AppLink to="/" className={overrideAppLink}>
           <IconTossBank />
         </AppLink>
@@ -34,28 +34,26 @@ function Header() {
 }
 
 const header = css`
+  background-color: #ffffff;
   position: fixed;
   width: 100%;
 
   z-index: var(--zIndex-fixed);
-`
-
-const inner = css`
-  height: var(--size-header-height);
-  max-width: 1140px;
-  padding: 0 48px;
-
-  background-color: #ffffff;
-
-  display: flex;
-  align-items: center;
-
   transition: box-shadow 0.2s ease;
-
   &.outline {
     box-shadow: 0 0 0.125rem rgb(0 0 0 / 22%),
       0 0.4375rem 0.625rem rgb(0 0 0 / 12%);
   }
+`
+
+const inner = css`
+  height: var(--size-header-height);
+  margin: 0 auto;
+  max-width: 1140px;
+  padding: 0 48px;
+
+  display: flex;
+  align-items: center;
 `
 
 const overrideAppLink = css`
